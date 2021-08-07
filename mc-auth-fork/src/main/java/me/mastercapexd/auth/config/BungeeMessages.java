61b4513d14a5ae7e276c81ffc0c90d50282b7e6c
Ubivashka <Ubivashka@gmail.com>
2021-08-07 16:31:36 +0600

    Added hex support, and some changes in config

--- mc-auth-fork/src/main/java/me/mastercapexd/auth/config/BungeeMessages.java
+++ mc-auth-fork/src/main/java/me/mastercapexd/auth/config/BungeeMessages.java
@@ -1,6 +1,9 @@
 package me.mastercapexd.auth.config;
 
+import java.awt.Color;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.common.collect.Maps;
 
@@ -11,14 +14,29 @@ import net.md_5.bungee.config.Configuration;
 public class BungeeMessages implements Messages {
 
 	private final Map<String, String> strings = Maps.newHashMap();
+	private final Pattern hexPattern = Pattern.compile("#([A-Fa-f0-9]){6}");
 
 	public BungeeMessages(Configuration messages) {
 		for (String key : messages.getKeys())
-			strings.put(key, ChatColor.translateAlternateColorCodes('&', messages.getString(key)));
+			strings.put(key, applyColor(ChatColor.translateAlternateColorCodes('&', messages.getString(key))));
+
 	}
 
 	@Override
 	public String getLegacyMessage(String key) {
 		return strings.get(key);
+	}
+
+	private String applyColor(String message) {
+		Matcher matcher = hexPattern.matcher(message);
+		while (matcher.find()) {
+			final ChatColor hexColor = ChatColor
+					.of(Color.decode(matcher.group().substring(0, matcher.group().length())));
+			final String before = message.substring(0, matcher.start());
+			final String after = message.substring(matcher.end());
+			message = before + hexColor + after;
+			matcher = hexPattern.matcher(message);
+		}
+		return message;
 	}
 }
