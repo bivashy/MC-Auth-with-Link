4b7ccf16a0523c43ce892845d32407602bf80060
Ubivashka <Ubivashka@gmail.com>
2021-08-09 15:43:30 +0600

    Added support messages for admin panel accounts

--- mc-auth-fork/src/main/java/me/mastercapexd/auth/vk/VKAccountsPageType.java
+++ mc-auth-fork/src/main/java/me/mastercapexd/auth/vk/VKAccountsPageType.java
@@ -1,5 +1,21 @@
 package me.mastercapexd.auth.vk;
 
 public enum VKAccountsPageType {
-	OWNPAGE, ALLACCOUNTSPAGE, ALLLINKEDACCOUNTSPAGE
+    OWNPAGE("no-accounts", "accounts"), ALLACCOUNTSPAGE("admin-panel-no-accounts", "admin-panel-accounts"),
+    ALLLINKEDACCOUNTSPAGE("admin-panel-no-linked-accounts", "admin-panel-linked-accounts");
+
+    private final String noAccountsPath, accountsPath;
+
+    private VKAccountsPageType(String noAccountsPath, String accountsPath) {
+        this.noAccountsPath = noAccountsPath;
+        this.accountsPath = accountsPath;
+    }
+
+    public String getNoAccountsPath() {
+        return noAccountsPath;
+    }
+
+    public String getAccountsPath() {
+        return accountsPath;
+    }
 }
