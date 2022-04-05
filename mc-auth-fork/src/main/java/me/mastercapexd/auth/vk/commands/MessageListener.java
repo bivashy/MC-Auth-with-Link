185ad53064f347a02a368935de06fc185449fcf1
Ubivashka <Ubivashka@gmail.com>
2022-04-05 18:09:51 +0600

    Thread pool for the Lamp command dispatching

--- mc-auth-fork/src/main/java/me/mastercapexd/auth/vk/commands/MessageListener.java
+++ mc-auth-fork/src/main/java/me/mastercapexd/auth/vk/commands/MessageListener.java
@@ -1,5 +1,8 @@
 package me.mastercapexd.auth.vk.commands;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import com.ubivashka.lamp.commands.vk.core.BaseVkActor;
 import com.ubivashka.lamp.commands.vk.core.VkHandler;
 import com.ubivashka.vk.bungee.events.VKMessageEvent;
@@ -11,12 +14,17 @@ import revxrsal.commands.command.ArgumentStack;
 import revxrsal.commands.command.CommandActor;
 
 public class MessageListener implements Listener {
+	private static final ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool();
+
 	@EventHandler
 	public void onMessage(VKMessageEvent event) {
-		VkHandler.getInstances().forEach((commandHandler) -> {
-			CommandActor commandActor = new VKCommandActorWrapper(new BaseVkActor(event.getMessage(), commandHandler));
-			ArgumentStack argumentStack = ArgumentStack.fromString(event.getMessage().getText());
-			commandHandler.dispatch(commandActor, argumentStack);
+		EXECUTOR_SERVICE.execute(() -> {
+			VkHandler.getInstances().forEach((commandHandler) -> {
+				CommandActor commandActor = new VKCommandActorWrapper(
+						new BaseVkActor(event.getMessage(), commandHandler));
+				ArgumentStack argumentStack = ArgumentStack.fromString(event.getMessage().getText());
+				commandHandler.dispatch(commandActor, argumentStack);
+			});
 		});
 	}
 }
