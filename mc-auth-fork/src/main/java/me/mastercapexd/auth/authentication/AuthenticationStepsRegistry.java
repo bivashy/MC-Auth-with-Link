e6546ed3243756d495790c23a663e4c9cc7c8a89
Ubivashka <Ubivashka@gmail.com>
2022-04-08 21:46:06 +0600

    Remove unused class

--- mc-auth-fork/src/main/java/me/mastercapexd/auth/authentication/AuthenticationStepsRegistry.java
+++
@@ -1,7 +0,0 @@
-package me.mastercapexd.auth.authentication;
-
-import me.mastercapexd.auth.bungee.AuthPlugin;
-
-public class AuthenticationStepsRegistry {
-	private static final AuthPlugin PLUGIN = AuthPlugin.getInstance();
-}
