178c72440c73e6c6bfe9dd9446790b910c8f6692
Ubivashka <Ubivashka@gmail.com>
2022-04-09 22:47:56 +0600

    Oops. Added missing GoogleLinkUser

---
+++ mc-auth-fork/src/main/java/me/mastercapexd/auth/link/google/GoogleLinkUser.java
@@ -0,0 +1,24 @@
+package me.mastercapexd.auth.link.google;
+
+import me.mastercapexd.auth.account.Account;
+import me.mastercapexd.auth.link.user.AbstractLinkUser;
+import me.mastercapexd.auth.link.user.info.LinkUserInfo;
+
+public class GoogleLinkUser extends AbstractLinkUser {
+	private GoogleLinkUserInfo linkInfoAccount;
+
+	public GoogleLinkUser(Account account, String googleKey, boolean confirmationEnabled) {
+		super(GoogleLinkType.getInstance(), account);
+		this.linkInfoAccount = new GoogleLinkUserInfo(googleKey, confirmationEnabled);
+	}
+	
+	public GoogleLinkUser(Account account, String googleKey) {
+		super(GoogleLinkType.getInstance(), account);
+		this.linkInfoAccount = new GoogleLinkUserInfo(googleKey);
+	}
+
+	@Override
+	public LinkUserInfo getLinkUserInfo() {
+		return linkInfoAccount;
+	}
+}
