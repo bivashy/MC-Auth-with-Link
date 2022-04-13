e367afa7f7a7737205bece9a1b0a3b4d9f69f73b
Ubivashka <Ubivashka@gmail.com>
2022-04-13 17:54:11 +0600

    Remove unused imports

--- mc-auth-fork/src/main/java/me/mastercapexd/auth/proxy/message/MultiProxyComponent.java
+++ mc-auth-fork/src/main/java/me/mastercapexd/auth/proxy/message/MultiProxyComponent.java
@@ -1,8 +1,5 @@
 package me.mastercapexd.auth.proxy.message;
 
-import net.kyori.adventure.text.TranslatableComponent;
-import net.md_5.bungee.api.chat.BaseComponent;
-
 /**
  * Component that have many other components, for example
  * {@link TranslatableComponent}, or {@link BaseComponent} array.
