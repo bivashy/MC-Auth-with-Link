fbba1877ac7afd872ea0301cde388d109a974531
Ubivashka <Ubivashka@gmail.com>
2022-04-17 15:25:46 +0600

    Add implementation of GoogleUnlinkCommand in messenger

--- mc-auth-fork/src/main/java/me/mastercapexd/auth/messenger/commands/GoogleUnlinkCommand.java
+++ mc-auth-fork/src/main/java/me/mastercapexd/auth/messenger/commands/GoogleUnlinkCommand.java
@@ -1,7 +1,38 @@
 package me.mastercapexd.auth.messenger.commands;
 
+import me.mastercapexd.auth.account.Account;
+import me.mastercapexd.auth.account.factories.AccountFactory;
+import me.mastercapexd.auth.config.PluginConfig;
+import me.mastercapexd.auth.link.LinkCommandActorWrapper;
+import me.mastercapexd.auth.link.LinkType;
+import me.mastercapexd.auth.link.google.GoogleLinkType;
+import me.mastercapexd.auth.link.google.GoogleLinkUser;
+import me.mastercapexd.auth.link.user.LinkUser;
+import me.mastercapexd.auth.proxy.commands.annotations.GoogleUse;
+import me.mastercapexd.auth.storage.AccountStorage;
+import revxrsal.commands.annotation.Default;
+import revxrsal.commands.annotation.Dependency;
 import revxrsal.commands.orphan.OrphanCommand;
 
 public class GoogleUnlinkCommand implements OrphanCommand {
+	@Dependency
+	private PluginConfig config;
+	@Dependency
+	private AccountStorage accountStorage;
 
+	@GoogleUse
+	@Default
+	public void unlink(LinkCommandActorWrapper actorWrapper, LinkType linkType, Account account) {
+		LinkUser linkUser = account.findFirstLinkUser(GoogleLinkType.LINK_USER_FILTER)
+				.orElse(new GoogleLinkUser(account, AccountFactory.DEFAULT_GOOGLE_KEY));
+
+		String linkUserKey = linkUser.getLinkUserInfo().getIdentificator().asString();
+		if (linkUserKey == null || linkUserKey.equals(AccountFactory.DEFAULT_GOOGLE_KEY) || linkUserKey.isEmpty()) {
+			actorWrapper.reply(linkType.getLinkMessages().getStringMessage("google-unlink-not-have-google"));
+			return;
+		}
+		actorWrapper.reply(linkType.getLinkMessages().getStringMessage("google-unlinked"));
+		linkUser.getLinkUserInfo().getIdentificator().setString(GoogleLinkType.NULL_KEY);
+		accountStorage.saveOrUpdateAccount(account);
+	}
 }
