00112afc28b5f73de09fe6d2b5d4469099ee3390
Ubivashka <Ubivashka@gmail.com>
2022-04-18 16:36:50 +0600

    Remove bungee import from proxy MultiProxyComponent interface

--- mc-auth-fork/src/main/java/me/mastercapexd/auth/proxy/message/MultiProxyComponent.java
+++ mc-auth-fork/src/main/java/me/mastercapexd/auth/proxy/message/MultiProxyComponent.java
@@ -1,7 +1,5 @@
 package me.mastercapexd.auth.proxy.message;
 
-import net.md_5.bungee.api.chat.BaseComponent;
-
 /**
  * Component that have many other components, for example
  * {@link TranslatableComponent}, or {@link BaseComponent} array.
