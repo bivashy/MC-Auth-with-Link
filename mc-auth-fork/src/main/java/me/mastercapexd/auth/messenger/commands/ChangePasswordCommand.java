301846a231ab9dbf22cd08c3d28478b20f2ebd31
Ubivashka <Ubivashka@gmail.com>
2022-04-21 16:40:37 +0600

    Added ChangePassword command

--- mc-auth-fork/src/main/java/me/mastercapexd/auth/messenger/commands/ChangePasswordCommand.java
+++ mc-auth-fork/src/main/java/me/mastercapexd/auth/messenger/commands/ChangePasswordCommand.java
@@ -1,7 +1,19 @@
 package me.mastercapexd.auth.messenger.commands;
 
+import me.mastercapexd.auth.account.Account;
+import me.mastercapexd.auth.link.LinkCommandActorWrapper;
+import me.mastercapexd.auth.link.LinkType;
+import me.mastercapexd.auth.proxy.commands.parameters.NewPassword;
+import revxrsal.commands.annotation.Default;
 import revxrsal.commands.orphan.OrphanCommand;
 
 public class ChangePasswordCommand implements OrphanCommand {
+	@Default
+	public void onPasswordChange(LinkCommandActorWrapper actorWrapper, LinkType linkType, Account player,
+			NewPassword newPassword) {
+		player.setPasswordHash(player.getHashType().hash(newPassword.getNewPassword()));
+		actorWrapper.reply(linkType.getLinkMessages().getStringMessage("changepass-success")
+				.replaceAll("(?i)%password%", newPassword.getNewPassword()));
 
+	}
 }
