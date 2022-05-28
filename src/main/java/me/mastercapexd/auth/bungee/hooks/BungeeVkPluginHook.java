3aff1023480148cd6acd09819b66c33a6e59671a
Ubivashka <Ubivashka@gmail.com>
2022-05-28 11:21:39 +0600

    Fix ClassNotFound exception because of Vk plugin hook

--- src/main/java/me/mastercapexd/auth/bungee/hooks/BungeeVkPluginHook.java
+++ src/main/java/me/mastercapexd/auth/bungee/hooks/BungeeVkPluginHook.java
@@ -1,6 +1,5 @@
 package me.mastercapexd.auth.bungee.hooks;
 
-import com.ubivashka.vk.api.providers.VkApiProvider;
 import com.ubivashka.vk.bungee.BungeeVkApiPlugin;
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.GroupActor;
@@ -9,8 +8,6 @@ import me.mastercapexd.auth.hooks.VkPluginHook;
 import me.mastercapexd.auth.proxy.ProxyPlugin;
 
 public class BungeeVkPluginHook implements VkPluginHook {
-	private static final VkApiProvider VK_API_PROVIDER = BungeeVkApiPlugin.getInstance().getVkApiProvider();
-
 	@Override
 	public boolean canHook() {
 		return ProxyPlugin.instance().getConfig().getVKSettings().isEnabled();
@@ -18,11 +15,11 @@ public class BungeeVkPluginHook implements VkPluginHook {
 
 	@Override
 	public VkApiClient getClient() {
-		return VK_API_PROVIDER.getVkApiClient();
+		return BungeeVkApiPlugin.getInstance().getVkApiProvider().getVkApiClient();
 	}
 
 	@Override
 	public GroupActor getActor() {
-		return VK_API_PROVIDER.getActor();
+		return BungeeVkApiPlugin.getInstance().getVkApiProvider().getActor();
 	}
 }
