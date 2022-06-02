4ea48ddb1ffad15da95022b626a6843e65ebbd98
Ubivashka <Ubivashka@gmail.com>
2022-06-02 12:36:43 +0600

    Fix permission annotation was not working

--- src/main/java/me/mastercapexd/auth/proxy/commands/annotations/Permission.java
+++ src/main/java/me/mastercapexd/auth/proxy/commands/annotations/Permission.java
@@ -6,6 +6,9 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import revxrsal.commands.annotation.DistributeOnMethods;
+
+@DistributeOnMethods
 @Retention(RUNTIME)
 @Target(TYPE)
 public @interface Permission {
