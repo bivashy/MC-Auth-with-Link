c76e750c7d9b36ff60e73061906f70e07d2a723b
Ubivashka <Ubivashka@gmail.com>
2022-06-02 16:11:27 +0600

    Add default value to AlterColumnUpdate

--- src/main/java/me/mastercapexd/auth/storage/column/AlterColumnUpdate.java
+++ src/main/java/me/mastercapexd/auth/storage/column/AlterColumnUpdate.java
@@ -6,10 +6,16 @@ import java.sql.SQLException;
 
 public class AlterColumnUpdate implements StorageUpdate {
 	private final String columnName, columnType;
+	private String defaultValue;
 
 	public AlterColumnUpdate(String columnName, String columnType) {
 		this.columnName = columnName;
 		this.columnType = columnType;
+	}
+
+	public AlterColumnUpdate(String columnName, String columnType, String defaultValue) {
+		this(columnName, columnType);
+		this.defaultValue = defaultValue;
 	}
 
 	@Override
@@ -17,6 +23,10 @@ public class AlterColumnUpdate implements StorageUpdate {
 		ResultSet resultSet = connection.getMetaData().getColumns(null, null, "auth", columnName);
 		if (resultSet.next())
 			return;
-		connection.createStatement().execute("ALTER TABLE `auth` ADD `" + columnName + "` " + columnType + ";");
+		if (defaultValue == null) {
+			connection.createStatement().execute("ALTER TABLE `auth` ADD `" + columnName + "` " + columnType + ";");
+		}else {
+			connection.createStatement().execute("ALTER TABLE `auth` ADD `" + columnName + "` " + columnType + " DEFAULT "+defaultValue+";");
+		}
 	}
 }
