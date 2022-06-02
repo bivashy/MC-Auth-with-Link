31c3e32af3cf7e68dbde78a45c56587c15fbe14d
Ubivashka <Ubivashka@gmail.com>
2022-06-02 16:11:06 +0600

    Check if user is administrator before sending admin panel

--- src/main/java/me/mastercapexd/auth/messenger/commands/AdminPanelCommand.java
+++ src/main/java/me/mastercapexd/auth/messenger/commands/AdminPanelCommand.java
@@ -10,6 +10,10 @@ import revxrsal.commands.orphan.OrphanCommand;
 public class AdminPanelCommand implements OrphanCommand {
 	@Default
 	public void adminPanelMenu(LinkCommandActorWrapper actorWrapper, LinkType linkType) {
+		if(!linkType.getSettings().isAdministrator(actorWrapper.userId())) {
+			actorWrapper.reply(linkType.getLinkMessages().getMessage("not-enough-permission"));
+			return;
+		}
 		Keyboard adminPanelKeyboard = linkType.getSettings().getKeyboards().createKeyboard("admin-panel");
 		actorWrapper.send(linkType.newMessageBuilder(linkType.getLinkMessages().getMessage("admin-panel"))
 				.keyboard(adminPanelKeyboard).build());
