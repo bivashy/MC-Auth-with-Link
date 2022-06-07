b4f086f953e32e90534834a0feada02545565927
U61vashka <botyrbojey@gmail.com>
2022-06-07 18:02:33 +0600

    Use properly link type for TelegramLinkEntryUser

--- src/main/java/me/mastercapexd/auth/link/entryuser/telegram/TelegramLinkEntryUser.java
+++ src/main/java/me/mastercapexd/auth/link/entryuser/telegram/TelegramLinkEntryUser.java
@@ -2,17 +2,17 @@ package me.mastercapexd.auth.link.entryuser.telegram;
 
 import me.mastercapexd.auth.account.Account;
 import me.mastercapexd.auth.link.entryuser.AbstractLinkEntryUser;
+import me.mastercapexd.auth.link.telegram.TelegramLinkType;
 import me.mastercapexd.auth.link.user.info.LinkUserInfo;
-import me.mastercapexd.auth.link.vk.VKLinkType;
 
 public class TelegramLinkEntryUser extends AbstractLinkEntryUser {
 
 	public TelegramLinkEntryUser(Account account, LinkUserInfo linkUserInfo) {
-		super(VKLinkType.getInstance(), account, linkUserInfo);
+		super(TelegramLinkType.getInstance(), account, linkUserInfo);
 	}
 
 	public TelegramLinkEntryUser(Account account) {
-		this(account, account.findFirstLinkUser(VKLinkType.LINK_USER_FILTER).get().getLinkUserInfo());
+		this(account, account.findFirstLinkUser(TelegramLinkType.LINK_USER_FILTER).get().getLinkUserInfo());
 	}
 
 }
