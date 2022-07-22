f4a31f9de44a849311405180df5ed5bdc00ae042
U61vashka <botyrbojey@gmail.com>
2022-07-22 19:05:48 +0600

    Fix ConfigurationArgumentError annotation was not working

--- src/main/java/me/mastercapexd/auth/messenger/commands/annotations/ConfigurationArgumentError.java
+++ src/main/java/me/mastercapexd/auth/messenger/commands/annotations/ConfigurationArgumentError.java
@@ -1,5 +1,12 @@
 package me.mastercapexd.auth.messenger.commands.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
 public @interface ConfigurationArgumentError {
     String value();
 }
