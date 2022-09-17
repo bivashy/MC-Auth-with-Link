a300b7fafc19dcef3b8059883c5ca622bf12e45f
U61vashka <botyrbojey@gmail.com>
2022-09-18 00:34:48 +0600

    Some small code beautify

--- core/src/main/java/me/mastercapexd/auth/config/DefaultPluginConfig.java
+++ core/src/main/java/me/mastercapexd/auth/config/DefaultPluginConfig.java
@@ -6,7 +6,7 @@ import com.ubivashka.configuration.holder.ConfigurationSectionHolder;
 import me.mastercapexd.auth.asset.resource.folder.FolderResourceReader;
 import me.mastercapexd.auth.proxy.ProxyPlugin;
 
-public class DefaultPluginConfig extends SpongeConfiguratePluginConfig{
+public class DefaultPluginConfig extends SpongeConfiguratePluginConfig {
     public DefaultPluginConfig(ProxyPlugin proxyPlugin) {
         super(proxyPlugin);
     }
