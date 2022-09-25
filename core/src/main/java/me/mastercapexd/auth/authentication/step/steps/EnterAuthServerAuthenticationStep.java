3d584d2fe6ee570924a415ef1e282a2aeddc05a1
U61vashka <botyrbojey@gmail.com>
2022-09-25 17:19:21 +0600

    Fix ENTER_AUTH_SERVER sometimes was not working

--- core/src/main/java/me/mastercapexd/auth/authentication/step/steps/EnterAuthServerAuthenticationStep.java
+++ core/src/main/java/me/mastercapexd/auth/authentication/step/steps/EnterAuthServerAuthenticationStep.java
@@ -1,11 +1,17 @@
 package me.mastercapexd.auth.authentication.step.steps;
 
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
 import me.mastercapexd.auth.Auth;
 import me.mastercapexd.auth.authentication.step.AbstractAuthenticationStep;
 import me.mastercapexd.auth.authentication.step.AuthenticationStep;
 import me.mastercapexd.auth.authentication.step.context.AuthenticationStepContext;
 import me.mastercapexd.auth.authentication.step.creators.AbstractAuthenticationStepCreator;
+import me.mastercapexd.auth.config.server.ConfigurationServer;
 import me.mastercapexd.auth.proxy.ProxyPlugin;
+import me.mastercapexd.auth.proxy.player.ProxyPlayer;
+import me.mastercapexd.auth.proxy.server.Server;
 
 public class EnterAuthServerAuthenticationStep extends AbstractAuthenticationStep {
     public static final String STEP_NAME = "ENTER_AUTH_SERVER";
@@ -26,8 +32,20 @@ public class EnterAuthServerAuthenticationStep extends AbstractAuthenticationSte
                 authenticationStepContext.getAccount().isSessionActive(PLUGIN.getConfig().getSessionDurability()))
             return true;
         authenticationStepContext.getAccount()
-                .getPlayer().ifPresent(PLUGIN.getConfig().findServerInfo(PLUGIN.getConfig().getAuthServers()).asProxyServer()::sendPlayer);
+                .getPlayer().ifPresent(this::tryToConnect);
         return true;
+    }
+
+    private void tryToConnect(ProxyPlayer player) {
+        ConfigurationServer foundServer = PLUGIN.getConfig().findServerInfo(PLUGIN.getConfig().getAuthServers());
+        Optional<Server> currentServerOptional = player.getCurrentServer();
+        if (!currentServerOptional.isPresent()) {
+            PLUGIN.getCore().schedule(PLUGIN, () -> tryToConnect(player), 3, TimeUnit.SECONDS);
+            return;
+        }
+        if (currentServerOptional.get().getServerName().equals(foundServer.getId()))
+            return;
+        foundServer.asProxyServer().sendPlayer(player);
     }
 
     public static class EnterAuthServerAuthenticationStepCreator extends AbstractAuthenticationStepCreator {
