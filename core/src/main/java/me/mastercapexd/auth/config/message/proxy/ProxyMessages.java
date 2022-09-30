48cfc5160e58431b58b7deb192e7d62baaa9924c
U61vashka <botyrbojey@gmail.com>
2022-09-30 20:33:00 +0600

    Fix #12

--- core/src/main/java/me/mastercapexd/auth/config/message/proxy/ProxyMessages.java
+++ core/src/main/java/me/mastercapexd/auth/config/message/proxy/ProxyMessages.java
@@ -11,23 +11,30 @@ import me.mastercapexd.auth.proxy.adventure.AdventureProxyComponent;
 import me.mastercapexd.auth.proxy.adventure.ComponentDeserializer;
 import me.mastercapexd.auth.proxy.message.ProxyComponent;
 
+import me.mastercapexd.auth.proxy.message.SelfHandledProxyComponent;
+
 public class ProxyMessages extends AbstractMessages<ProxyComponent> {
     private static final ProxyPlugin PLUGIN = ProxyPlugin.instance();
     @ConfigField("deserializer")
     private final ComponentDeserializer deserializer = ComponentDeserializer.LEGACY_AMPERSAND;
 
     public ProxyMessages(ConfigurationSectionHolder configurationSection) {
-        super(configurationSection, DEFAULT_DELIMITER, "");
+        super(configurationSection, DEFAULT_DELIMITER, null);
         ProxyPlugin.instance().getConfigurationProcessor().resolve(configurationSection, this);
     }
 
     @Override
     public ProxyComponent getMessage(String key, MessageContext context) {
-        return fromText(context.apply(getStringMessage(key)));
+        String message = getStringMessage(key);
+        if (message == null)
+            return SelfHandledProxyComponent.NULL_COMPONENT;
+        return fromText(context.apply(message));
     }
 
     @Override
     public ProxyComponent fromText(String text) {
+        if (text == null)
+            return SelfHandledProxyComponent.NULL_COMPONENT;
         return new AdventureProxyComponent(deserializer.deserialize(text));
     }
 
--- core/src/main/java/me/mastercapexd/auth/proxy/message/SelfHandledProxyComponent.java
+++ core/src/main/java/me/mastercapexd/auth/proxy/message/SelfHandledProxyComponent.java
@@ -2,6 +2,27 @@ package me.mastercapexd.auth.proxy.message;
 
 import me.mastercapexd.auth.proxy.player.ProxyPlayer;
 
-public interface SelfHandledProxyComponent extends ProxyComponent{
+public interface SelfHandledProxyComponent extends ProxyComponent {
+    SelfHandledProxyComponent NULL_COMPONENT = new SelfHandledProxyComponent() {
+        @Override
+        public void send(ProxyPlayer player) {
+        }
+
+        @Override
+        public String jsonText() {
+            return null;
+        }
+
+        @Override
+        public String legacyText() {
+            return null;
+        }
+
+        @Override
+        public String plainText() {
+            return null;
+        }
+    };
+
     void send(ProxyPlayer player);
 }
