e31e25a7ff9b4bcb49a10a2e85a8c1d7757d9430
U61vashka <botyrbojey@gmail.com>
2023-01-11 20:34:09 +0600

    Add default implementation of MessengerKeyboards#create

--- core/src/main/java/me/mastercapexd/auth/config/messenger/MessengerKeyboards.java
+++ core/src/main/java/me/mastercapexd/auth/config/messenger/MessengerKeyboards.java
@@ -1,7 +1,23 @@
 package me.mastercapexd.auth.config.messenger;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.google.gson.Gson;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+import com.ubivashka.messenger.telegram.message.keyboard.TelegramKeyboard;
 import com.ubivaska.messenger.common.keyboard.Keyboard;
 
+import me.mastercapexd.auth.adapter.ArrayPairHashMapAdapter;
+
 public interface MessengerKeyboards {
-    Keyboard createKeyboard(String key, String... placeholders);
+    Gson GSON = new Gson();
+    default Keyboard createKeyboard(String key, String... placeholders){
+        String rawJson = getRawJsonKeyboards().get(key);
+        for (Entry<String, String> entry : new ArrayPairHashMapAdapter<>(placeholders).entrySet())
+            rawJson = rawJson.replaceAll(entry.getKey(), entry.getValue());
+        return new TelegramKeyboard(GSON.fromJson(rawJson, InlineKeyboardMarkup.class));
+    }
+
+    Map<String,String> getRawJsonKeyboards();
 }
