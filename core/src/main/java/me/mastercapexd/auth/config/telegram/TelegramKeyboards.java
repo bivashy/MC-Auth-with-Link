3b2df89b2d34c647ad8d0c2d9955cd42d70b67b8
U61vashka <botyrbojey@gmail.com>
2023-01-11 20:32:41 +0600

    Get rid of the duplicate code on MessengerKeyboards#createKeyboard

--- core/src/main/java/me/mastercapexd/auth/config/telegram/TelegramKeyboards.java
+++ core/src/main/java/me/mastercapexd/auth/config/telegram/TelegramKeyboards.java
@@ -1,22 +1,16 @@
 package me.mastercapexd.auth.config.telegram;
 
+import java.util.Collections;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import com.google.gson.Gson;
-import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.ubivashka.configuration.ConfigurationHolder;
 import com.ubivashka.configuration.holder.ConfigurationSectionHolder;
-import com.ubivashka.messenger.telegram.message.keyboard.TelegramKeyboard;
-import com.ubivaska.messenger.common.keyboard.Keyboard;
 
 import me.mastercapexd.auth.config.messenger.MessengerKeyboards;
-import me.mastercapexd.auth.utils.CollectionUtils;
 
 public class TelegramKeyboards implements ConfigurationHolder, MessengerKeyboards {
-    private static final Gson GSON = new Gson();
     private final Map<String, String> jsonKeyboards;
 
     public TelegramKeyboards(ConfigurationSectionHolder sectionHolder) {
@@ -24,10 +18,7 @@ public class TelegramKeyboards implements ConfigurationHolder, MessengerKeyboard
     }
 
     @Override
-    public Keyboard createKeyboard(String key, String... placeholders) {
-        String rawJson = jsonKeyboards.get(key);
-        for (Entry<String, String> entry : CollectionUtils.createStringMap(placeholders).entrySet())
-            rawJson = rawJson.replaceAll(entry.getKey(), entry.getValue());
-        return new TelegramKeyboard(GSON.fromJson(rawJson, InlineKeyboardMarkup.class));
+    public Map<String, String> getRawJsonKeyboards() {
+        return Collections.unmodifiableMap(jsonKeyboards);
     }
 }
--- core/src/main/java/me/mastercapexd/auth/config/vk/VKKeyboards.java
+++ core/src/main/java/me/mastercapexd/auth/config/vk/VKKeyboards.java
@@ -1,21 +1,16 @@
 package me.mastercapexd.auth.config.vk;
 
+import java.util.Collections;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import com.google.gson.Gson;
 import com.ubivashka.configuration.ConfigurationHolder;
 import com.ubivashka.configuration.holder.ConfigurationSectionHolder;
-import com.ubivashka.messenger.vk.message.keyboard.VkKeyboard;
-import com.ubivaska.messenger.common.keyboard.Keyboard;
 
 import me.mastercapexd.auth.config.messenger.MessengerKeyboards;
-import me.mastercapexd.auth.utils.CollectionUtils;
 
 public class VKKeyboards implements ConfigurationHolder, MessengerKeyboards {
-    private static final Gson GSON = new Gson();
     private final Map<String, String> jsonKeyboards;
 
     public VKKeyboards(ConfigurationSectionHolder sectionHolder) {
@@ -23,10 +18,7 @@ public class VKKeyboards implements ConfigurationHolder, MessengerKeyboards {
     }
 
     @Override
-    public Keyboard createKeyboard(String key, String... placeholders) {
-        String rawJson = jsonKeyboards.get(key);
-        for (Entry<String, String> entry : CollectionUtils.createStringMap(placeholders).entrySet())
-            rawJson = rawJson.replaceAll(entry.getKey(), entry.getValue());
-        return new VkKeyboard(GSON.fromJson(rawJson, com.vk.api.sdk.objects.messages.Keyboard.class));
+    public Map<String, String> getRawJsonKeyboards() {
+        return Collections.unmodifiableMap(jsonKeyboards);
     }
 }
