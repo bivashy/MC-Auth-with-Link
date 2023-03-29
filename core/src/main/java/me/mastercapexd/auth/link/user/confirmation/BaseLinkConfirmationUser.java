076bce9b28dad570ea0e08c14d2bc94b06b15452
bivashy <botyrbojey@gmail.com>
2023-03-29 23:40:40 +0600

    Add LinkUserIdentificator, LinkType into LinkConfirmationUser

--- core/src/main/java/me/mastercapexd/auth/link/user/confirmation/BaseLinkConfirmationUser.java
+++ core/src/main/java/me/mastercapexd/auth/link/user/confirmation/BaseLinkConfirmationUser.java
@@ -1,18 +1,23 @@
 package me.mastercapexd.auth.link.user.confirmation;
 
 import com.bivashy.auth.api.account.Account;
+import com.bivashy.auth.api.link.LinkType;
 import com.bivashy.auth.api.link.user.confirmation.LinkConfirmationUser;
+import com.bivashy.auth.api.link.user.info.LinkUserIdentificator;
 import com.bivashy.auth.api.type.LinkConfirmationType;
 
 public class BaseLinkConfirmationUser implements LinkConfirmationUser {
     private final LinkConfirmationType linkConfirmationType;
     private final long linkTimeoutTimestamp;
+    private final LinkType linkType;
     private final Account target;
     private final String code;
+    private LinkUserIdentificator linkUserIdentificator;
 
-    public BaseLinkConfirmationUser(LinkConfirmationType linkConfirmationType, long linkTimeoutTimestamp, Account target, String code) {
+    public BaseLinkConfirmationUser(LinkConfirmationType linkConfirmationType, long linkTimeoutTimestamp, LinkType linkType, Account target, String code) {
         this.linkConfirmationType = linkConfirmationType;
         this.linkTimeoutTimestamp = linkTimeoutTimestamp;
+        this.linkType = linkType;
         this.target = target;
         this.code = code;
     }
@@ -35,5 +40,20 @@ public class BaseLinkConfirmationUser implements LinkConfirmationUser {
     @Override
     public long getLinkTimeoutTimestamp() {
         return linkTimeoutTimestamp;
+    }
+
+    @Override
+    public LinkType getLinkType() {
+        return linkType;
+    }
+
+    @Override
+    public LinkUserIdentificator getLinkUserIdentificator() {
+        return linkUserIdentificator;
+    }
+
+    @Override
+    public void setLinkUserIdentificator(LinkUserIdentificator linkUserIdentificator) {
+        this.linkUserIdentificator = linkUserIdentificator;
     }
 }
