7aa8aac5e24849a7ef6eec51d70f8ec722ef653b
bivashy <botyrbojey@gmail.com>
2023-03-29 22:38:53 +0600

    Add MessageableCommandActor superinterface to LinkCommandActorWrapper

--- core/src/main/java/me/mastercapexd/auth/link/LinkCommandActorWrapper.java
+++ core/src/main/java/me/mastercapexd/auth/link/LinkCommandActorWrapper.java
@@ -1,12 +1,19 @@
 package me.mastercapexd.auth.link;
 
 import com.bivashy.auth.api.link.user.info.LinkUserIdentificator;
+import com.bivashy.auth.api.shared.commands.MessageableCommandActor;
 import com.ubivaska.messenger.common.message.Message;
 
 import revxrsal.commands.command.CommandActor;
 
-public interface LinkCommandActorWrapper extends CommandActor {
+public interface LinkCommandActorWrapper extends CommandActor, MessageableCommandActor {
     void send(Message message);
 
     LinkUserIdentificator userId();
+
+    @Override
+    default void replyWithMessage(Object message) {
+        if (message instanceof String)
+            reply((String) message);
+    }
 }
