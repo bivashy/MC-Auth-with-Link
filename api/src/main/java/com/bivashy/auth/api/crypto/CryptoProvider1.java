6771904a1a8033eec841c5740ea3f236f010d177
bivashy <botyrbojey@gmail.com>
2023-05-27 22:29:44 +0600

    Remove old CryptoProvider enum

--- api/src/main/java/com/bivashy/auth/api/crypto/CryptoProvider1.java
+++
@@ -1,9 +0,0 @@
-package com.bivashy.auth.api.crypto;
-
-public interface CryptoProvider1 {
-    HashedPassword hash(HashInput input);
-
-    boolean matches(HashInput input, HashedPassword password);
-
-    String getIdentifier();
-}
