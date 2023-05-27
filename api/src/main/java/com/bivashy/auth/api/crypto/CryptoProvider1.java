3f90f48a7338f6a237fe39c4ee91799498739fe5
bivashy <botyrbojey@gmail.com>
2023-05-27 22:27:57 +0600

    Use HashedPassword as argument in CryptoProvider

--- api/src/main/java/com/bivashy/auth/api/crypto/CryptoProvider1.java
+++ api/src/main/java/com/bivashy/auth/api/crypto/CryptoProvider1.java
@@ -3,7 +3,7 @@ package com.bivashy.auth.api.crypto;
 public interface CryptoProvider1 {
     HashedPassword hash(HashInput input);
 
-    boolean matches(HashInput input, String hashedInput);
+    boolean matches(HashInput input, HashedPassword password);
 
     String getIdentifier();
 }
