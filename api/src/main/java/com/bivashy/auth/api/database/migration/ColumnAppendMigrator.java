8eb11cc51e93d096774d1cc58c6056d8b3bd5b37
bivashy <botyrbojey@gmail.com>
2023-05-27 23:24:17 +0600

    Add ColumnAppendMigrator#of

--- api/src/main/java/com/bivashy/auth/api/database/migration/ColumnAppendMigrator.java
+++ api/src/main/java/com/bivashy/auth/api/database/migration/ColumnAppendMigrator.java
@@ -2,11 +2,30 @@ package com.bivashy.auth.api.database.migration;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.function.Function;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.support.ConnectionSource;
 
 public interface ColumnAppendMigrator<T, ID> extends ConditionalMigrator<T, ID> {
+    static <T, ID> ColumnAppendMigrator<T, ID> of(String columnName, Function<ConnectionSource, String> columnTypeFunction) {
+        return new ColumnAppendMigrator<T, ID>() {
+            @Override
+            public String getColumnName() {
+                return columnName;
+            }
+
+            @Override
+            public String getColumnType(ConnectionSource connectionSource) {
+                return columnTypeFunction.apply(connectionSource);
+            }
+        };
+    }
+
+    static <T, ID> ColumnAppendMigrator<T, ID> of(String columnName, String columnType) {
+        return of(columnName, ignored -> columnType);
+    }
+
     @Override
     default boolean shouldMigrate(ConnectionSource connectionSource, Dao<? extends T, ID> dao) throws SQLException {
         ResultSet balanceColumnResultSet = connectionSource.getReadOnlyConnection(dao.getTableName())
