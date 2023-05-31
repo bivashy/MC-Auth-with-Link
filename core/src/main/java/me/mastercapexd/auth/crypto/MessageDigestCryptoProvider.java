36b3aa3f05dc0e42f78524f5c2392bd44c2fca2e
bivashy <botyrbojey@gmail.com>
2023-06-01 04:10:41 +0600

    Make SHA256, MD5 backward compatible

--- core/src/main/java/me/mastercapexd/auth/crypto/MessageDigestCryptoProvider.java
+++ core/src/main/java/me/mastercapexd/auth/crypto/MessageDigestCryptoProvider.java
@@ -1,12 +1,14 @@
 package me.mastercapexd.auth.crypto;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
 import com.bivashy.auth.api.crypto.HashInput;
 import com.bivashy.auth.api.crypto.HashedPassword;
-import me.mastercapexd.auth.util.HashUtils;
 
 public class MessageDigestCryptoProvider extends BaseCryptoProvider {
+    private static final String FORMAT = "%032x";
     private final MessageDigest digest;
 
     public MessageDigestCryptoProvider(String identifier, MessageDigest digest) {
@@ -16,6 +18,11 @@ public class MessageDigestCryptoProvider extends BaseCryptoProvider {
 
     @Override
     protected HashedPassword hashInput(HashInput input, String salt) {
-        return HashedPassword.of(HashUtils.hashText(input.getRawInput(), digest), salt, this);
+        return HashedPassword.of(hash(input.getRawInput()), salt, this);
+    }
+
+    // We are using this hashing for backward compatibility
+    private String hash(String input) {
+        return String.format(FORMAT, new BigInteger(1, digest.digest(input.getBytes(StandardCharsets.UTF_8))));
     }
 }
