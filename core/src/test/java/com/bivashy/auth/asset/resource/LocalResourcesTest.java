f2ec69df76c4f05c91d265e7b413d5e9d7a97842
bivashy <botyrbojey@gmail.com>
2023-06-06 01:21:39 +0600

    Make tests for FolderResource, DefaultResource

---
+++ core/src/test/java/com/bivashy/auth/asset/resource/LocalResourcesTest.java
@@ -0,0 +1,7 @@
+package com.bivashy.auth.asset.resource;
+
+public class LocalResourcesTest extends ResourcesTest {
+    public LocalResourcesTest() {
+        super(LocalResourcesTest.class.getClassLoader());
+    }
+}
