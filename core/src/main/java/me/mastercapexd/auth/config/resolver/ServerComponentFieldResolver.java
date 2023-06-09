0c9806b8e1f4329c68e96695867326142e5b208a
bivashy <botyrbojey@gmail.com>
2023-06-09 22:54:09 +0600

    Rename ProxyComponentFieldResolver, use ServerComponent#from for resolving ServerComponent

--- core/src/main/java/me/mastercapexd/auth/config/resolver/ServerComponentFieldResolver.java
+++ core/src/main/java/me/mastercapexd/auth/config/resolver/ServerComponentFieldResolver.java
@@ -2,32 +2,29 @@ package me.mastercapexd.auth.config.resolver;
 
 import java.util.Arrays;
 
-import com.bivashy.auth.api.AuthPlugin;
 import com.bivashy.auth.api.server.message.ServerComponent;
 import com.bivashy.configuration.context.ConfigurationFieldResolverContext;
 import com.bivashy.configuration.holder.ConfigurationSectionHolder;
 import com.bivashy.configuration.resolver.field.ConfigurationFieldResolver;
 
-public class ProxyComponentFieldResolver implements ConfigurationFieldResolver<ServerComponent> {
+public class ServerComponentFieldResolver implements ConfigurationFieldResolver<ServerComponent> {
     @Override
     public ServerComponent resolveField(ConfigurationFieldResolverContext context) {
-        AuthPlugin proxyPlugin = AuthPlugin.instance();
         if (context.isSection()) {
             ConfigurationSectionHolder sectionHolder = context.getSection();
             String componentType = sectionHolder.getString("type");
             switch (componentType) {
                 case "json":
-                    return proxyPlugin.getCore().componentJson(sectionHolder.getString("value"));
+                    return ServerComponent.fromJson(sectionHolder.getString("value"));
                 case "legacy":
-                    return proxyPlugin.getCore().componentLegacy(sectionHolder.getString("value"));
+                    return ServerComponent.fromLegacy(sectionHolder.getString("value"));
                 case "plain":
-                    return proxyPlugin.getCore().componentPlain(sectionHolder.getString("value"));
+                    return ServerComponent.fromPlain(sectionHolder.getString("value"));
                 default:
                     throw new IllegalArgumentException(
                             "Illegal component type in " + Arrays.toString(context.path()) + ":" + componentType + ", available: json,legacy,plain");
             }
         }
-        return proxyPlugin.getCore()
-                .componentLegacy(context.getString());
+        return ServerComponent.fromLegacy(context.getString());
     }
 }
