b00eea7d8adff18dce0445a58e05282dd2b662e1
bivashy <botyrbojey@gmail.com>
2023-06-26 18:22:38 +0600

    Add BaseDiscordSettings into PluginConfigTemplate

--- core/src/main/java/me/mastercapexd/auth/config/discord/DiscordKeyboards.java
+++ core/src/main/java/me/mastercapexd/auth/config/discord/DiscordKeyboards.java
@@ -1,6 +1,7 @@
 package me.mastercapexd.auth.config.discord;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -10,6 +11,9 @@ import com.bivashy.configuration.ConfigurationHolder;
 import com.bivashy.configuration.holder.ConfigurationSectionHolder;
 import com.bivashy.messenger.common.keyboard.Keyboard;
 import com.bivashy.messenger.discord.message.keyboard.DiscordKeyboard;
+import com.google.gson.reflect.TypeToken;
+
+import net.dv8tion.jda.api.interactions.components.ActionRow;
 
 public class DiscordKeyboards implements ConfigurationHolder, LinkKeyboards {
     private final Map<String, String> jsonKeyboards;
@@ -25,6 +29,7 @@ public class DiscordKeyboards implements ConfigurationHolder, LinkKeyboards {
 
     @Override
     public Keyboard createKeyboardModel(String rawJson) {
-        return new DiscordKeyboard(GSON.fromJson(rawJson, new TypeToken<List<ActionRow>>()));
+        return new DiscordKeyboard(GSON.fromJson(rawJson, new TypeToken<List<ActionRow>>() {
+        }.getType()));
     }
 }
