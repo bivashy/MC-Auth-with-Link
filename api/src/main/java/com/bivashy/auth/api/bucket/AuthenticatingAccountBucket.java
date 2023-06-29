6995009457f08190cc595aa827c175509804f5d9
bivashy <botyrbojey@gmail.com>
2023-06-29 17:50:19 +0600

    Deprecate AuthenticatingAccountBucket methods named authorizing

--- api/src/main/java/com/bivashy/auth/api/bucket/AuthenticatingAccountBucket.java
+++ api/src/main/java/com/bivashy/auth/api/bucket/AuthenticatingAccountBucket.java
@@ -9,10 +9,13 @@ import com.bivashy.auth.api.model.PlayerIdSupplier;
 public interface AuthenticatingAccountBucket {
     Collection<String> getAccountIdEntries();
 
+    @Deprecated
     boolean isAuthorizing(PlayerIdSupplier playerIdSupplier);
 
+    @Deprecated
     Optional<Account> getAuthorizingAccount(PlayerIdSupplier playerIdSupplier);
 
+    @Deprecated
     default Account getAuthorizingAccountNullable(PlayerIdSupplier playerIdSupplier) {
         return getAuthorizingAccount(playerIdSupplier).orElse(null);
     }
@@ -23,7 +26,9 @@ public interface AuthenticatingAccountBucket {
         return getEnterTimestamp(playerIdSupplier).orElse(0L);
     }
 
+    @Deprecated
     void addAuthorizingAccount(Account account);
 
+    @Deprecated
     void removeAuthorizingAccount(PlayerIdSupplier playerIdSupplier);
 }
