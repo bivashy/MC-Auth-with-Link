b48cf1af50202d4742818917b5993c6ff2564900
bivashy <botyrbojey@gmail.com>
2023-08-15 01:56:03 +0600

    Added BaseListBucket, BaseModifiableListBucket

---
+++ core/src/main/java/me/mastercapexd/auth/bucket/BaseListBucket.java
@@ -0,0 +1,38 @@
+package me.mastercapexd.auth.bucket;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import com.bivashy.auth.api.bucket.Bucket;
+import com.bivashy.auth.api.bucket.ModifiableBucket;
+
+public class BaseListBucket<T> implements Bucket<T> {
+    private final List<T> list;
+    private final ModifiableBucket<T> modifiableBucket;
+
+    public BaseListBucket(List<T> list, ModifiableBucket<T> modifiableBucket) {
+        this.list = list;
+        this.modifiableBucket = modifiableBucket;
+    }
+
+    public BaseListBucket(List<T> list) {
+        this(list, new BaseModifiableListBucket<>(list));
+    }
+
+    @Override
+    public Collection<T> getUnmodifiableRaw() {
+        return Collections.unmodifiableList(list);
+    }
+
+    @Override
+    public Stream<T> stream() {
+        return list.stream();
+    }
+
+    @Override
+    public ModifiableBucket<T> modifiable() {
+        return modifiableBucket;
+    }
+}
