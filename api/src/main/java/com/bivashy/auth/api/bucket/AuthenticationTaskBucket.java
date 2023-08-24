470baef027dddd7c61875d8cf7bc2678b0a8a01d
bivashy <botyrbojey@gmail.com>
2023-08-24 18:52:53 +0600

    Move getTasks into AuthenticationTaskBucket interface from base class

--- api/src/main/java/com/bivashy/auth/api/bucket/AuthenticationTaskBucket.java
+++ api/src/main/java/com/bivashy/auth/api/bucket/AuthenticationTaskBucket.java
@@ -1,20 +1,25 @@
 package com.bivashy.auth.api.bucket;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.bivashy.auth.api.model.AuthenticationTask;
 
 public interface AuthenticationTaskBucket extends Bucket<AuthenticationTask> {
+
     @Deprecated
-    default void addTask(AuthenticationTask task){
+    default void addTask(AuthenticationTask task) {
         modifiable().add(task);
     }
 
     @Deprecated
-    default void removeTask(AuthenticationTask task){
+    default void removeTask(AuthenticationTask task) {
         modifiable().remove(task);
     }
 
     @Deprecated
-    List<AuthenticationTask> getTasks();
+    default List<AuthenticationTask> getTasks() {
+        return new ArrayList<>(getUnmodifiableRaw());
+    }
+
 }
--- core/src/main/java/me/mastercapexd/auth/bucket/BaseAuthenticationTaskBucket.java
+++ core/src/main/java/me/mastercapexd/auth/bucket/BaseAuthenticationTaskBucket.java
@@ -1,15 +1,8 @@
 package me.mastercapexd.auth.bucket;
 
-import java.util.List;
-
 import com.bivashy.auth.api.bucket.AuthenticationTaskBucket;
 import com.bivashy.auth.api.model.AuthenticationTask;
 
 public class BaseAuthenticationTaskBucket extends BaseListBucket<AuthenticationTask> implements AuthenticationTaskBucket {
-
-    @Override
-    public List<AuthenticationTask> getTasks() {
-        return (List<AuthenticationTask>) getUnmodifiableRaw();
-    }
 
 }
