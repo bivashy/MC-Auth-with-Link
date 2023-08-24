312c182e0dc10f87cab64fe29fde86459035c037
bivashy <botyrbojey@gmail.com>
2023-08-24 18:54:20 +0600

    Remove redundant constructor in BaseCryptoProviderBucket

--- core/src/main/java/me/mastercapexd/auth/bucket/BaseCryptoProviderBucket.java
+++ core/src/main/java/me/mastercapexd/auth/bucket/BaseCryptoProviderBucket.java
@@ -1,14 +1,8 @@
 package me.mastercapexd.auth.bucket;
 
-import java.util.ArrayList;
-
 import com.bivashy.auth.api.bucket.CryptoProviderBucket;
 import com.bivashy.auth.api.crypto.CryptoProvider;
 
 public class BaseCryptoProviderBucket extends BaseListBucket<CryptoProvider> implements CryptoProviderBucket {
-
-    public BaseCryptoProviderBucket() {
-        super(new ArrayList<>());
-    }
 
 }
