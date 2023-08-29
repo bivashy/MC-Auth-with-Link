a58e59e5103dc33645f3149279fdb96467a58723
bivashy <botyrbojey@gmail.com>
2023-08-29 17:02:45 +0600

    Replace Eclipse Aether with Apache Maven Artifact Resolver

--- core/src/main/java/me/mastercapexd/auth/util/MavenArtifactUtil.java
+++ core/src/main/java/me/mastercapexd/auth/util/MavenArtifactUtil.java
@@ -10,9 +10,7 @@ import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.collection.CollectRequest;
-import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
 import org.eclipse.aether.graph.Dependency;
-import org.eclipse.aether.impl.DefaultServiceLocator;
 import org.eclipse.aether.repository.LocalRepository;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.resolution.ArtifactDescriptorException;
@@ -22,13 +20,10 @@ import org.eclipse.aether.resolution.ArtifactResult;
 import org.eclipse.aether.resolution.DependencyRequest;
 import org.eclipse.aether.resolution.DependencyResolutionException;
 import org.eclipse.aether.resolution.DependencyResult;
-import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
-import org.eclipse.aether.spi.connector.transport.TransporterFactory;
-import org.eclipse.aether.spi.log.LoggerFactory;
-import org.eclipse.aether.transport.file.FileTransporterFactory;
-import org.eclipse.aether.transport.http.HttpTransporterFactory;
+import org.eclipse.aether.supplier.RepositorySystemSupplier;
 
 public class MavenArtifactUtil {
+
     private static final RepositorySystem REPOSITORY_SYSTEM = newRepositorySystem();
     private static final RepositorySystemSession REPOSITORY_SYSTEM_SESSION = newRepositorySystemSession(REPOSITORY_SYSTEM);
 
@@ -67,7 +62,7 @@ public class MavenArtifactUtil {
         return new RemoteRepository.Builder(id, "default", url).build();
     }
 
-    private static DefaultRepositorySystemSession newRepositorySystemSession(RepositorySystem system) {
+    private static RepositorySystemSession newRepositorySystemSession(RepositorySystem system) {
         DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
 
         LocalRepository localRepo = new LocalRepository("target/local-repo");
@@ -77,18 +72,7 @@ public class MavenArtifactUtil {
     }
 
     private static RepositorySystem newRepositorySystem() {
-        DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
-        locator.addService(RepositoryConnectorFactory.class, BasicRepositoryConnectorFactory.class);
-        locator.addService(TransporterFactory.class, FileTransporterFactory.class);
-        locator.addService(TransporterFactory.class, HttpTransporterFactory.class);
-        locator.setErrorHandler(new DefaultServiceLocator.ErrorHandler() {
-            @Override
-            public void serviceCreationFailed(Class<?> type, Class<?> impl, Throwable exception) {
-                exception.printStackTrace();
-            }
-        });
-        // Disable logging
-        locator.setServices(LoggerFactory.class, (LoggerFactory[]) null);
-        return locator.getService(RepositorySystem.class);
+        return new RepositorySystemSupplier().get();
     }
+
 }
