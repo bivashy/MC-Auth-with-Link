62fc881343be24dafc05b56b629a3f91a751dca5
bivashy <botyrbojey@gmail.com>
2023-11-02 16:18:39 +0600

    Provide AuthAccount in AccountLinkAdapter

--- core/src/main/java/me/mastercapexd/auth/database/adapter/AccountLinkAdapter.java
+++ core/src/main/java/me/mastercapexd/auth/database/adapter/AccountLinkAdapter.java
@@ -3,12 +3,13 @@ package me.mastercapexd.auth.database.adapter;
 import com.bivashy.auth.api.link.user.LinkUser;
 
 import me.mastercapexd.auth.database.model.AccountLink;
+import me.mastercapexd.auth.database.model.AuthAccount;
 
 public class AccountLinkAdapter extends AccountLink {
 
-    public AccountLinkAdapter(LinkUser linkUser) {
+    public AccountLinkAdapter(LinkUser linkUser, AuthAccount account) {
         super(linkUser.getLinkType().getName(), linkUser.getLinkUserInfo().getIdentificator().asString(), linkUser.getLinkUserInfo().isConfirmationEnabled(),
-                null);
+                account);
     }
 
 }
